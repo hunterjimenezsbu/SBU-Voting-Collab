021fd417ab2a0b0a1f789f16b8c0ef7550033edc
hunterjimenezsbu <44508183+hunterjimenezsbu@users.noreply.github.com>
2020-05-05 15:45:18 -0400

    Update MenuScreen.js

--- src/screens/MenuScreen.js
+++ src/screens/MenuScreen.js
@@ -5,7 +5,7 @@ export default class Menu extends Component {
         return (
             <div className = "buttonHolder">
                 <Button id = "checkYourVoter" className = "menuButton" onClick = {(e)=>this.props.changeScreen('voterStatus')}>Check your Voter Status</Button>
-                <Button id = "registerToVote" className = "menuButton" onClick = {(e)=>this.props.changeScreen('registerToVote')}>Register to Vote</Button>
+                <Button id = "registerToVote" className = "menuButton" onClick = {(e)=>this.props.changeScreen('OpenCampus')}>Register to Vote</Button>
                 <Button id = "absenteeVoting" className = "menuButton" onClick = {(e)=>this.props.changeScreen('absenteeVoting')}>Absentee Voting</Button>
                 <Button id = "chatWithTeam" className = "menuButton" onClick = {(e)=>this.props.changeScreen('chat')}>Chat with a Team Member</Button>
             </div>
